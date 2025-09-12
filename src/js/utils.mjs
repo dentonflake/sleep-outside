2f994665cfd9ce139bc9c5ba97e8eb21b78382e3
Denton Flake <me@dentonflake.com>
2025-09-12 06:34:33 -0700

    Bug fixes

--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -21,9 +21,20 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
-//to return a parameter from the URL when requested
+
+// get the product id from the query string
 export function getParam(param) {
   const queryString = window.location.search;
   const urlParams = new URLSearchParams(queryString);
-  return urlParams.get(param)
+  const product = urlParams.get(param);
+  return product
+}
+
+export function renderListWithTemplate(template, parentElement, list, position = "afterbegin", clear = false) {
+  const htmlStrings = list.map(template);
+  // if clear is true we need to clear out the contents of the parent.
+  if (clear) {
+    parentElement.innerHTML = "";
+  }
+  parentElement.insertAdjacentHTML(position, htmlStrings.join(""));
 }
