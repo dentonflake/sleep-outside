62c46abff219f47e12dd22c06bccdf60178b700e
Denton Flake <me@dentonflake.com>
2025-09-19 10:22:35 -0700

    Bug fixes

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-import { loadHeaderFooter } from './utils.mjs';
+import { loadHeaderFooter } from "./utils.mjs";
 
 const dataSource = new ProductData("tents");
 
@@ -8,6 +8,6 @@ const element = document.querySelector(".product-list");
 
 const productList = new ProductList("Tents", dataSource, element);
 
-// productList.init();
+productList.init();
 
 loadHeaderFooter()
