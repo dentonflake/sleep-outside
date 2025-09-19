a2d851e27b4dee74db702371df719d6e051f1ab2
Denton Flake <me@dentonflake.com>
2025-09-19 10:15:55 -0700

    Completed activity

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,6 @@
 import ProductData from "./ProductData.mjs";
 import ProductList from "./ProductList.mjs";
-
-
+import { loadHeaderFooter } from './utils.mjs';
 
 const dataSource = new ProductData("tents");
 
@@ -9,4 +8,6 @@ const element = document.querySelector(".product-list");
 
 const productList = new ProductList("Tents", dataSource, element);
 
-productList.init();
+// productList.init();
+
+loadHeaderFooter()
